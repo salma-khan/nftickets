ae86868264bb524d74eff5c30e43df7bf6cea1a9
salma-khan <salmakhanchouch@MacBook-Air-de-Salma.local>
2023-11-26 17:10:37 +0100

    add arbitrum sepolia configuration.

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
+require("@nomicfoundation/hardhat-verify")
+
+const API_URL = process.env.API_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    arbitrum_sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 421614,
+
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  solidity: {
+    compilers: [
+      {
+      version: "0.8.20"
+      }
+    ]
+  }
+
 };
