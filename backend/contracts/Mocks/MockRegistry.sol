8001560ccb13ff849392995518283ba8deffa7cb
salma-khan <salmakhanchouch@MacBook-Air-de-Salma.local>
2023-12-03 21:45:20 +0100

    secure Upkeeper and add unit tests.

---
+++ backend/contracts/Mocks/MockRegistry.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+
+
+contract MockRegistry {
+   
+    function getForwarder(uint256 upkeepId) external view returns (address){
+       return address(bytes20(keccak256(abi.encode(block.timestamp))));
+    }
+
+}
