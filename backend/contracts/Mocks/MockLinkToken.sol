140b01edb4c36bf6e6c625af15c3461564abf921
salma-khan <salmakhanchouch@MacBook-Air-de-Salma.local>
2023-12-03 15:18:02 +0100

    Mock chainLink

---
+++ backend/contracts/Mocks/MockLinkToken.sol
@@ -0,0 +1,28 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+
+
+contract MockLinkToken {
+    event LinkTransferred(uint256 amount);
+    event Approved();
+
+    function transfer(address to, uint256 value) external returns (bool success){
+        emit LinkTransferred(value);
+    }
+
+    function approve(address spender, uint256 value) external returns (bool success) {
+      emit Approved();
+    }
+
+    function decimals() external view returns (uint8 decimalPlaces){
+        return 1;
+    }
+
+   
+    function balanceOf(address adr) external view returns( uint256) {
+        return 5;
+    }
+
+}
---
+++ backend/contracts/Mocks/MockRegistrar.sol
@@ -0,0 +1,24 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+import "./../AutomationRegistrarInterface.sol";
+
+contract MockRegistrar is AutomationRegistrarInterface {
+    function registerUpkeep(
+        RegistrationParams calldata requestParams
+    ) external returns (uint256){
+       return 1;
+     
+    }
+
+}
+
+contract MockRegistrarError is AutomationRegistrarInterface {
+    function registerUpkeep(
+        RegistrationParams calldata requestParams
+    ) external returns (uint256){
+      return 0;
+    }
+
+}
+
